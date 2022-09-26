d5628e861442528233aa12124a77930115f4d883
pryaniki <skoro11@bk.ru>
2022-09-26 20:13:44 +0300

    Create models in transport.

--- transportgtaonline/transport/models.py
+++ transportgtaonline/transport/models.py
@@ -4,3 +4,50 @@ from .abstracts import BaseTable
 
 class Brand(BaseTable):
     pass
+
+
+class Update(BaseTable):
+    date = models.DateField()
+
+# Ground
+
+
+class GroundCategory(BaseTable):
+    pass
+
+
+class Garage(BaseTable):
+    owner = models.ManyToManyField('users.CustomUser', on_delete=models.CASCADE, null=True)
+
+
+class GroundWishList(BaseTable):
+    owner = models.OneToOneField('users.CustomUser', on_delete=models.CASCADE, null=True)
+
+# Air
+
+
+class AirCategory(BaseTable):
+    pass
+
+
+class Hangar(BaseTable):
+    owner = models.ManyToManyField('users.CustomUser', on_delete=models.CASCADE, null=True)
+
+
+class AirWishList(BaseTable):
+    owner = models.OneToOneField('users.CustomUser', on_delete=models.CASCADE, null=True)
+
+# Water
+
+
+class WaterCategory(BaseTable):
+    pass
+
+
+class Port(BaseTable):
+    owner = models.OneToOneField('users.CustomUser', on_delete=models.CASCADE, null=True)
+
+
+class WaterWishList(BaseTable):
+    owner = models.OneToOneField('users.CustomUser', on_delete=models.CASCADE, null=True)
+
